e423182a79eb93262ff5623557e082e2c0f7fdfa
FranciscoRomero23 <4afromero@gmail.com>
2018-11-15 21:13:27 +0100

    Probando a insertar registros en una zona inversa de pruebas

--- gestionDNS.sh
+++ gestionDNS.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 zonadirecta=db.iesgn.org
+zonainversa=db.192.168.10
 
 if [ $1 = "-a" ]
 then
 	if [ $2 = "-dir" ]
 	then
+		ip=`echo $4 | cut -d"." -f4`
+		dominio=`sed -e '/ORIGIN/ !d' $zonadirecta | cut -d" " -f2`
+
 		echo "$3	IN	A	$4" >> $zonadirecta
+		echo "$ip	IN	PTR	$3.$dominio" >> $zonainversa
 	elif [ $2 = "-alias" ]
 	then
-		echo "$3	IN	CNAME	$4"
+		echo "$3	IN	CNAME	$4" >> $zonadirecta
 	else
 		echo "Paramentros incorrectos."
 	fi
