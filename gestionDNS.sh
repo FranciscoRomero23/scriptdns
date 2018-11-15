b81e4ed313dc97a96a142f724f1c999a395a57d5
FranciscoRomero23 <4afromero@gmail.com>
2018-11-15 20:21:50 +0100

    Probando inserciones en una zona directa de pruebas

--- gestionDNS.sh
+++ gestionDNS.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+zonadirecta=db.iesgn.org
+
 if [ $1 = "-a" ]
 then
 	if [ $2 = "-dir" ]
 	then
-		echo "$3	IN	A	$4"
+		echo "$3	IN	A	$4" >> $zonadirecta
 	elif [ $2 = "-alias" ]
 	then
 		echo "$3	IN	CNAME	$4"
