2f3fd023562319ae555c8afd8f6ec3165e74e6b4
FranciscoRomero23 <4afromero@gmail.com>
2018-11-15 11:15:43 +0100

    Creando la base del script

---
+++ gestionDNS.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $1 = "-a" ]
+then
+	if [ $2 = "-dir" ]
+	then
+		echo "$3	IN	A	$4"
+	elif [ $2 = "-alias" ]
+	then
+		echo "$3	IN	CNAME	$4"
+	else
+		echo "Paramentros incorrectos."
+	fi
+elif [ $1 = "-b" ]
+then
+	echo "Borrar"
+else
+	echo "Incorrecto"
+fi
